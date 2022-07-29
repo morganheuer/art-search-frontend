53c0298b2151267ec6ffc0b24a2e2a6cc87db73f
Morgan <morganheuer@gmail.com>
2022-07-29 11:53:53 -0700

    SearchArt returns first 10 results

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,11 @@
 import React from "react";
 import "./App.css";
 import SearchArt from "./components/SearchArt";
-import axios from "axios";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [query, setQuery] = useState("Vincent van Gogh");
-  const [data, setData] = useState([]);
-  const [image, setImage] = useState("");
-  const getAllArt = () => {
-    axios
-      .get(
-        `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}&hasImages=true&isPublicDomain=true`
-      )
-      .then((res) => {
-        console.log(res.data.objectIDs);
-        setData(res.data.objectIDs.slice(0, 10));
-        getImage();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const getImage = () => {
-    axios
-      .get(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${data[4]}`
-      )
-      .then((res) => {
-        console.log(res.data.primaryImageSmall);
-        setImage(res.data.primaryImageSmall);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  // getImage(436529);
-  useEffect(() => {
-    getAllArt();
-  }, []);
-  const displayData = () => {
-    return data ? (
-      data.map((data) => {
-        return (
-          <div>
-            <h3>{data}</h3>
-          </div>
-        );
-      })
-    ) : (
-      <h3>No data yet</h3>
-    );
-  };
-
   return (
     <div className="App">
       <SearchArt />
-      <img src={image} alt="artwork"></img>
-      <div>{displayData()}</div>
     </div>
   );
 }
--- src/components/SearchArt.tsx
+++ src/components/SearchArt.tsx
@@ -1,13 +1,67 @@
+import { useState } from "react";
+import axios from "axios";
 const SearchArt = () => {
-    return (
-        <div>
-            <form>
-                <label htmlFor="query"></label>
-                <input type="text" name="query" />
-                <button type="submit">Search</button>
-            </form>
-        </div>
-    )
+  const [query, setQuery] = useState("");
+  const [objects, setObjects] = useState([]);
+  const [images, setImages] = useState<string[]>([]);
+
+  const getObjectsFromSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}&hasImages=true&isPublicDomain=true`
+      )
+      .then((res) => {
+        const objectIDs = res.data.objectIDs.slice(0, 10);
+        setObjects(objectIDs);
+        getImages();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getImages = () => {
+    let img: string[] = [];
+    for (let i of objects) {
+      axios
+        .get(
+          `https://collectionapi.metmuseum.org/public/collection/v1/objects/${i}`
+        )
+        .then((res) => {
+          let imgsrc = res.data.primaryImageSmall;
+          console.log(imgsrc);
+          if (imgsrc) {
+            img.push(imgsrc);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    setImages(img);
+  };
+
+  return (
+    <div>
+      <form onSubmit={getObjectsFromSearch}>
+        <label htmlFor="query"></label>
+        <input
+          type="text"
+          name="query"
+          placeholder={`Try "Monet" or "Van Gogh"`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        {images.map((image) => (
+          <img alt="artwork" src={image} width="20%"></img>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SearchArt;
