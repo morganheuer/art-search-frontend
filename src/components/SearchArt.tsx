ceb3e8a8365863c11d48ffb7c1a6b285e2d4cfa7
Morgan <morganheuer@gmail.com>
2022-07-28 17:20:37 -0700

    initial commit

---
+++ src/components/SearchArt.tsx
@@ -0,0 +1,13 @@
+const SearchArt = () => {
+    return (
+        <div>
+            <form>
+                <label htmlFor="query"></label>
+                <input type="text" name="query" />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    )
+};
+
+export default SearchArt;
